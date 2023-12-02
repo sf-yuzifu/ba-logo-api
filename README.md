3b19b550aef7705740d85ac608af5f72e2e79b1e
小鱼yuzifu <yuzifu@yuzifu.top>
2023-12-02 18:45:30 +0800

    [add]more params

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # BA Logo API
 
 An api of [BlueArchive-style Logo Generator](https://github.com/nulla2011/bluearchive-logo) (Using Puppeteer)
+
+# Params
+
+- l : `string` Left word of the logo
+- r : `string` Right word of the logo
+- x : `string` the X pos of the halo
+- y : `string` the X pos of the halo
+- tp : `boolean` whether is transparent background
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000;
-const cors = require('cors')
+// 导入所需库
 const puppeteer = require("puppeteer");
 const {readdir, createReadStream, existsSync, readdirSync, statSync, unlinkSync, rmdirSync} = require("fs");
 const {join} = require("path");
-
+// 初始化API参数
 let textL = "小鱼";
 let textR = "档案";
-
+let x = "-15";
+let y = "0";
+let tp = false;
+// Sleep函数
 const sleep = (timeout) => {
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -16,8 +16,8 @@ const sleep = (timeout) => {
         }, timeout)
     })
 }
-
-function _deleteDir(url) {
+// 删除文件夹
+const _deleteDir = (url) => {
     let files = [];
     if (existsSync(url)) {
         files = readdirSync(url);
@@ -35,18 +35,34 @@ function _deleteDir(url) {
         console.log('路径不存在！');
     }
 }
-
+// 服务器配置
+const express = require('express')
+const app = express()
+const port = 3000;
+const cors = require('cors')
 app.use(cors())
-
 app.listen(port, () => {
     console.log(`Server is running on http://127.0.0.1:${port}/`)
 });
 
+/**
+ * 主程序，其中
+ * @param {string} l Logo左边文字
+ * @param {string} r Logo右边文字
+ * @param {string} x 光圈X坐标
+ * @param {string} y 光圈Y坐标
+ * @param {boolean} tp 是否透明背景
+ */
 app.get('/balogo', async function (ctx, res) {
-    let random = Math.random().toString(36).slice(-8);
     console.log(ctx.query);
+
+    let random = Math.random().toString(36).slice(-8);
     textL = ctx.query.l || "";
     textR = ctx.query.r || "";
+    x = ctx.query.x || "-15";
+    y = ctx.query.y || "0";
+    tp = ctx.query.tp || false;
+
     const browser = await puppeteer.launch({
         headless: true
     });
@@ -57,17 +73,23 @@ app.get('/balogo', async function (ctx, res) {
         behavior: 'allow',
         downloadPath: `./pic/${random}/`
     });
+    await page.waitForSelector('canvas');
+    if (tp === "true") {
+        await page.evaluate(() => {
+            document.querySelector("#transparent").click()
+        });
+    }
     await page.evaluate(() => {
         document.querySelector("#textL").value = "";
         document.querySelector("#textR").value = "";
+        document.querySelector("#graphX").value = "";
+        document.querySelector("#graphY").value = "";
+        document.querySelector(".collapse.collapse-arrow.bg-base-100 input").click()
     });
-    await page.waitForSelector('canvas');
-    await page.type('#textL', textL, {
-        delay: 0,
-    });
-    await page.type('#textR', textR, {
-        delay: 0,
-    });
+    await page.type('#graphX', x);
+    await page.type('#graphY', y);
+    await page.type('#textL', textL);
+    await page.type('#textR', textR);
     await page.waitForSelector('#loading.hidden');
     await page.waitForSelector('#loading:not(.hidden)');
     await page.waitForSelector('#loading.hidden');
